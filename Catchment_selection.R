a35f28485cd5bc063e845ef967cd1018144b442b
Rodrigo Aguayo <roodaguayo@gmail.com>
2024-06-17 10:33:02 +0200

    basin selection based on data (1990-2019)

---
+++ Catchment_selection.R
@@ -0,0 +1,37 @@
+# Catchment selection  --------------------------------------------------------------
+
+rm(list=ls())
+cat("\014")  
+
+library("airGR")
+library("hydroTSM")
+library("hydroGOF")
+library("hydroPSO")
+library("terra")
+library("exactextractr")
+library("sf")
+
+options(xts_check_TZ = FALSE)
+setwd("/home/rooda/OneDrive/Patagonia/")
+
+# threshold
+threshold <- 0.75
+
+# time period
+period_calib    <- seq(as.POSIXct("1990-01-01", tz = "UTC"), 
+                       as.POSIXct("2019-12-31", tz = "UTC"), by = "day")
+
+# Basin data
+q_metadata <- read.csv("Data/Zenodo/v11/Q_PMETobs_v11_metadata.csv", row.names = "gauge_id")
+q_metadata$gauge_code <- as.character(q_metadata$gauge_code)
+q_obs      <- read.csv("Data/Zenodo/v11/Q_PMETobs_1950_2020_v11d.csv", row.names = "Date")
+q_obs      <- q_obs[rownames(q_obs) %in% as.character(period_calib), ] 
+q_shape    <- vect("GIS South/Basins_PMETobs_points.shp")
+
+# subset
+basin_select <- colSums(!is.na(q_obs))/nrow(q_obs)
+q_metadata   <- q_metadata[(basin_select > threshold),] 
+q_shape   <- q_shape[(basin_select > threshold),] 
+
+# save
+writeVector(q_shape, "GIS South/Basins_PMETobs_points_subset.shp", overwrite = TRUE)
