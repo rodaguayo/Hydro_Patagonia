8c7731e7b8be4fd1859914c98052f27e794545e7
Rodrigo Aguayo <roodaguayo@gmail.com>
2024-09-10 11:46:07 +0200

    new directories + dhdt as attribute

--- Catchment_attributes.R
+++ Catchment_attributes.R
@@ -7,16 +7,18 @@ library("terra")
 library("zoo")
 library("sf")
 
-setwd("/home/rooda/Dropbox/Patagonia/")
-terra::gdalCache(20000)
+terra::gdalCache(40000)
+
+setwd("/home/rooda/OneDrive/DeepHydro/")
+path_pmet <- "/home/rooda/OneDrive/PatagoniaMet/"
 
 # Basins to simulate
-basin_shp  <- st_read("GIS South/Basins_Patagonia_all.shp")
+basin_shp  <- st_read("GIS/Basins_Patagonia_all.shp")
 
 # Reference period
 period <- c(as.POSIXct("1989-12-31"), as.POSIXct("2019/12/31"))
 
-dem <- rast("GIS South/dem_patagonia3f.tif")
+dem <- rast(paste0(path_pmet, "GIS/dem_patagonia3f.tif"))
 dem <- subst(dem, NA, 0)  # NAs to sea level (= 0)
 slope <- terrain(dem, v='aspect', unit='degrees')
 
@@ -26,37 +28,47 @@ basin_shp$elev_median <- round(exact_extract(dem,   basin_shp, "median"), 1)
 basin_shp$slope_mean  <- round(exact_extract(slope, basin_shp, "median"), 1)
 
 # forest cover
-forest_cover <- rast("GIS South/lc_forest_500m.tif")
+forest_cover <- rast(paste0(path_pmet, "GIS/lc_forest_500m.tif"))
 forest_cover <- subst(forest_cover, NA, 0)  # NAs to 0 %
 basin_shp$forest_cover <- round(exact_extract(forest_cover, basin_shp, "mean"), 1)
 
 # lake cover
-lake_cover <- rast("GIS South/lc_water_500m.tif")
+lake_cover <- rast(paste0(path_pmet, "GIS/lc_water_500m.tif"))
 lake_cover <- subst(lake_cover, NA, 0)  # NAs to 0 %
 basin_shp$lake_cover   <- round(exact_extract(lake_cover, basin_shp, "mean"), 1)
 
 # glacier cover (RGI7)
-glaciers  <- vect("GIS South/Glaciers/RGI7.shp")
+glaciers  <- vect(paste0(path_pmet, "GIS/Glaciers/RGI7.shp"))
 glaciers  <- subset(glaciers, glaciers$cenlat <= -40.5)
 glaciers  <- rasterize(glaciers, dem, background = 0) * 100
 basin_shp$glacier_cover <- round(exact_extract(glaciers,   basin_shp, "mean"), 1)
 
+# Glacier change (elevation in m y-1)
+dh_dt <- rast(paste0(path_pmet, "GIS/Glaciers/dhdt_2021.tif"))
+dh_dt <- project(dh_dt, crs(basin_shp))
+dh_dt <- mask(dh_dt, vect(basin_shp), overwrite=TRUE)
+dh_dt <- crop(dh_dt, vect(basin_shp))*1000 # from m to mm
+dh_dt <- dh_dt / 1.091 # from ice to water
+dh_dt[is.na(dh_dt)] <- 0
+
+basin_shp$glacier_dhdt <- round(exact_extract(dh_dt, basin_shp, "mean"), 1)
+
 # leaf area index (LAI)
-lai_data   <- rast("GIS South/LAI_climatology_1981-2015.nc4")
+lai_data   <- rast(paste0(path_pmet, "GIS/LAI_climatology_1981-2015.nc4"))
 lai_data   <- subst(lai_data, NA, 0)  # NAs to 0 %
 lai_data   <- round(exact_extract(lai_data, basin_shp, "mean"), 3)
 basin_shp$lai_max  <- apply(lai_data, 1, max, na.rm=TRUE)
 basin_shp$lai_diff <- apply(lai_data, 1, max, na.rm=TRUE) - apply(lai_data, 1, min, na.rm=TRUE)
 
 # p_mean from PMET v1.1
-pp_stack <- rast("Data/Precipitation/PP_PMETsim_1980_2020_v10m.nc")
+pp_stack <- rast(paste0(path_pmet, "Zenodo/v11/PP_PMETsim_1980_2020_v11d.nc"))
 terra::time(pp_stack) <- as.POSIXct(time(pp_stack), tz= "UTC") 
 pp_stack <- subset(pp_stack, which(time(pp_stack) > period[1] & time(pp_stack) <= period[2]))
 pp_stack <- mean(tapp(pp_stack, strftime(time(pp_stack), format="%Y"), fun = sum, na.rm = TRUE))
 basin_shp$p_mean_PMET <- round(exact_extract(pp_stack, basin_shp, "mean"), 1)
 
 # pet_mean from PMET v1.1 (Hargreaves eq based on temp)
-pet_stack <- rast("Data/Evapotranspiration/Ep_PMET_1980_2020d.nc")
+pet_stack <- rast(paste0(path_pmet, "Data/Evaporation/Ep_PMETsim_1980_2020d_dev.nc"))
 terra::time(pet_stack) <- as.POSIXct(time(pet_stack), tz= "UTC") 
 pet_stack <- subset(pet_stack, which(time(pet_stack) > period[1] & time(pet_stack) <= period[2]))
 pet_stack <- mean(tapp(pet_stack, strftime(time(pet_stack),format="%Y"), fun = sum, na.rm = TRUE))
@@ -66,7 +78,7 @@ basin_shp$pet_mean_PMET <- round(exact_extract(pet_stack, basin_shp, "mean"), 1)
 basin_shp$aridity_PMET <- round(basin_shp$p_mean_PMET/basin_shp$pet_mean_PMET, 3)
 
 # precipitation data for each basin
-pp_stack <- rast("Data/Precipitation/PP_PMETsim_1980_2020_v10d.nc")
+pp_stack <- rast(paste0(path_pmet, "Zenodo/v11/PP_PMETsim_1980_2020_v11d.nc"))
 terra::time(pp_stack) <- as.POSIXct(time(pp_stack), tz= "UTC") 
 pp_stack <- subset(pp_stack, which(time(pp_stack) > period[1] & time(pp_stack) <= period[2]))
 
@@ -77,7 +89,7 @@ colnames(pp_stack)  <- basin_shp$gauge_id
 pp_stack <- zoo(pp_stack,  order.by = time_pp)
 
 # temperature data for each basin
-t2m_stack <- rast("Data/Temperature/Tavg_PMETsim_1980_2020_v10d.nc")
+t2m_stack <- rast(paste0(path_pmet, "Zenodo/v11/Tavg_PMETsim_1980_2020_v11d.nc"))
 terra::time(t2m_stack) <- as.POSIXct(time(t2m_stack), tz= "UTC") 
 t2m_stack <- subset(t2m_stack, which(time(t2m_stack) > period[1] & time(t2m_stack) <= period[2]))
 
@@ -117,5 +129,5 @@ pp_snow  <- aggregate(pp_stack, by = list(substr(row.names(pp_stack),1,4)), FUN 
 pp_snow  <- colMeans(pp_snow[,-1])
 basin_shp$frac_snow_PMET     <- round(pp_snow/pp_total, 3)
 
-writeVector(vect(basin_shp), "GIS South/Basins_Patagonia_all_data.shp", overwrite=TRUE)
-write.csv(st_drop_geometry(basin_shp), "MS3 Results/Attributes_all_basins.csv")
+writeVector(vect(basin_shp), "GIS/Basins_Patagonia_all_data.shp", overwrite=TRUE)
+write.csv(st_drop_geometry(basin_shp), "Attributes_all_basins.csv", row.names = FALSE)
